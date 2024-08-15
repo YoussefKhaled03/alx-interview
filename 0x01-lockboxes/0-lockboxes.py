f666f1ee426077a3245c1c814f3a7f69e585f8ed
{YoussefKhaled03} <{ykte03@gmail.com}>
2024-08-16 01:32:58 +0300

    edit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,22 +2,31 @@
 '''LockBoxes Challenge'''
 
 
-def can_unlock_all(boxes):
-    """
-    Checks if all the boxes in the given list can be unlocked.
-    Args:
-        boxes (list): A list of lists representing the lockboxes and their corresponding keys.
+def canUnlockAll(boxes):
+    '''determines if all the boxes can be opened or not
     Returns:
-        bool: True if all the boxes can be unlocked, False otherwise.
-    """
-    opened_boxes = set([0])
-    stack = [0]
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key not in opened_boxes and key < len(boxes):
-                opened_boxes.add(key)
-                stack.append(key)
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    return len(opened_boxes) == len(boxes)
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
