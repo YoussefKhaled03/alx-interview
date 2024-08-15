c08020204571705ac049a2cdc70923edf19009ac
{YoussefKhaled03} <{ykte03@gmail.com}>
2024-08-16 01:31:56 +0300

    feat: refactor canUnlockAll function for improved readability and performance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-def canUnlockAll(boxes):
+'''LockBoxes Challenge'''
+
+
+def can_unlock_all(boxes):
+    """
+    Checks if all the boxes in the given list can be unlocked.
+    Args:
+        boxes (list): A list of lists representing the lockboxes and their corresponding keys.
+    Returns:
+        bool: True if all the boxes can be unlocked, False otherwise.
+    """
     opened_boxes = set([0])
     stack = [0]
 
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-canUnlockAll = __import__('0-lockboxes').canUnlockAll
+can_unlock_all = __import__('0-lockboxes').can_unlock_all
 
 boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+print(can_unlock_all(boxes))
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
+print(can_unlock_all(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+print(can_unlock_all(boxes))
