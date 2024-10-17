85aab06ec4255105a95244255c0830e378b01e28
Youssef Khaled <122903999+YoussefKhaled03@users.noreply.github.com>
2024-10-18 01:32:21 +0200

    feat: refactor and rename functions for prime game logic; improve documentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,50 +1,44 @@
 #!/usr/bin/python3
-def sieve_of_eratosthenes(n):
-    """Generate a list of prime numbers up to n using the Sieve of Eratosthenes."""
-    is_prime = [True] * (n + 1)
-    p = 2
-    while p * p <= n:
-        if is_prime[p]:
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-        p += 1
-    prime_numbers = [p for p in range(2, n + 1) if is_prime[p]]
-    return prime_numbers
+"""
+Define isWineer function, a solution to the Prime Game problem
+"""
 
-def is_winner(x, nums):
-    """Determine the winner of the prime game."""
-    if x <= 0 or not nums:
+
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Arguements:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
+def isWinner(x, nums):
+    """
+    Determines winner of Prime Game
+    Arguments:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    
-    maria_wins = 0
-    ben_wins = 0
-    
-    for n in nums:
-        if n == 1:
-            ben_wins += 1
-            continue
-        
-        primes = sieve_of_eratosthenes(n)
-        primes_set = set(primes)
-        current_player = "Maria"
-        
-        while primes_set:
-            prime = min(primes_set)
-            multiples = set(range(prime, n + 1, prime))
-            primes_set -= multiples
-            
-            if not primes_set:
-                if current_player == "Maria":
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-            
-            current_player = "Ben" if current_player == "Maria" else "Maria"
-    
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
